1f3af0f6347b61e72b98de3a8ed60b18abbaac6c
Moraguma <g170603@dac.unicamp.br>
2023-11-05 00:54:45 -0300

    Started work on voting

--- scenes/scripts/Screens.gd
+++ scenes/scripts/Screens.gd
@@ -4,7 +4,8 @@ extends Node2D
 const PHASE_POSITIONS = {
 	"NIGHT": Vector2(1080, 0),
 	"DAY_ANNOUNCEMENTS": Vector2(-1080, 0),
-	"DISCUSSION": Vector2(0, -1920)
+	"DISCUSSION": Vector2(0, -1920),
+	"VOTING": Vector2(0, 1920)
 }
 
 
